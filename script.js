52a355ab9ef011cce13ae201f3e619056f049e35
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-01-03 23:11:14 +0530

    box added with css

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const allboxes=[-1,-1,-1,-1,-1,-1,-1,-1];
+const board=[-1,-1,x,-1,-1,-1,o,-1];
+
+function renderboard(){
+   const allboxes= document.querySelectorAll('.box');
+
+   allboxes.forEach((boxelement,i)=>{
+    if(board[i]=='x')
+    {
+     boxelement.innerHTML='pqr';
+    }
+    else if(board[i]=='o'){
+     boxelement.innerHTML='<img src=./../images/star.png />';
+    }
+    else{
+    boxelement.innerHTML='';
+    }
+   })
+}
+renderboard();
+
+function play(num)
