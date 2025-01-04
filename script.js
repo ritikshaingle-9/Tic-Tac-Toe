c118b790f6c214e73fa3115cc12df8565f5f8df6
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-01-04 18:33:24 +0530

    add player function added

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
-const board=[-1,-1,x,-1,-1,-1,o,-1];
+const board=[-1,-1,-1,-1,-1,-1,-1,-1];
+let currentplayer='x';
 
 function renderboard(){
    const allboxes= document.querySelectorAll('.box');
 
    allboxes.forEach((boxelement,i)=>{
-    if(board[i]=='x')
+    if(board[i]==="x")
     {
-     boxelement.innerHTML='pqr';
+     boxelement.innerHTML='<img src=./../images/moon.png height="90px" width="90px"/>';
     }
-    else if(board[i]=='o'){
-     boxelement.innerHTML='<img src=./../images/star.png />';
+    else if(board[i]==="o"){
+     boxelement.innerHTML='<img src=./../images/star.png height="90px" width="90px"/>';
     }
     else{
     boxelement.innerHTML='';
     }
-   })
+   });
 }
 renderboard();
 
-function play(num)
+function addplayer(){
+const playerone = document.getElementById('playerone');
+const playertwo = document.getElementById('playertwo');
+const nameofoneplayer = playerone.value;
+const nameoftwoplayer = playertwo.value;
+localStorage.setItem("player 1",nameofoneplayer);
+localStorage.setItem("player 2",nameoftwoplayer);
+}
+
+function play(num){
+   board[num]= currentplayer;
+   renderboard();
+   const name=document.getElementById('currentplayer');
+
+   if(currentplayer ==="x"){
+      currentplayer="o";
+      name.innerHTML=localStorage.getItem("player 1");
+   }
+   else{
+      currentplayer ="x";
+       name.innerHTML=localStorage.getItem("player 2");
+   }
+}
